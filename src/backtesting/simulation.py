ecb8e93fce6b5339b78f9eafd905d404b8ecf22e
Mitchell-Medeiros <mitch_med@yahoo.com>
2023-04-13 22:00:48 +0000

    Consolidated compare.py and simulation.py. Added lists for storing sharpe ratio and max drawdown values to provide full stats for these metrics. Made all lists into dataframes to be combined into the dash table. Removed obsolete print statements.

--- src/backtesting/simulation.py
+++ src/backtesting/simulation.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from statistics import mean
 import vectorbt as vbt
-from dash import dash_table
 
 from . data import num_windows, num_days, in_price, out_price
 from . strategies import ind
@@ -41,20 +40,22 @@ for i in range(num_windows):
     )
 
     # Saves the optimized values for inputing into the out-of-sample windows and other key data.
-    average_return_values.append(round(pf.total_return().mean()*100,4))
-    max_return_values.append(round(pf.total_return().max()*100,4))
+    average_return_values.append(round(pf.total_return().mean()*100,3))
+    max_return_values.append(round(pf.total_return().max()*100,3))
     max_return_params.append(pf.total_return().idxmax())
 
-    average_sharpe_values.append(round(pf.sharpe_ratio().mean()*100,4))
-    max_sharpe_values.append(round(pf.sharpe_ratio().max()*100,4))
+    average_sharpe_values.append(round(pf.sharpe_ratio().mean(),3))
+    max_sharpe_values.append(round(pf.sharpe_ratio().max(),3))
     max_sharpe_params.append(pf.sharpe_ratio().idxmax())
 
-    average_maxdrawdown_values.append(round(pf.max_drawdown().mean()*100,4))
-    min_maxdrawdown_values.append(round(pf.max_drawdown().min()*100,4))
+    average_maxdrawdown_values.append(round(pf.max_drawdown().mean()*100,3))
+    min_maxdrawdown_values.append(round(pf.max_drawdown().min()*100,3))
     min_maxdrawdown_params.append(pf.max_drawdown().idxmin())
 
 
-realized_returns, missed_returns = [],[]
+realized_returns, difference_in_returns = [],[]
+realized_sharpe, difference_in_sharpe = [],[]
+realized_maxdrawdown, difference_in_maxdrawdown = [],[]
 
 # Loops the indicator on the out-of-sample windows, inputting the parameters that
 # maximimized our chosen metric for the corresponding in-sample windows.
@@ -78,26 +79,125 @@ for i in range(num_windows):
         **pf_kwargs
     )
 
-    realized_returns.append(round(pf_t.total_return()*100,4))
-    missed_returns.append(round(max_return_values[i]-realized_returns[i],4))
-    mean_ad = np.sum(np.abs(realized_returns[i]-np.mean(realized_returns)))/(i+1)
+    realized_returns.append(round(pf_t.total_return()*100,3))
+    difference_in_returns.append(round(max_return_values[i]-realized_returns[i],3))
+
+    realized_sharpe.append(round(pf_t.sharpe_ratio(),3))
+    difference_in_sharpe.append(round(max_sharpe_values[i]-realized_sharpe[i],3))
+
+    realized_maxdrawdown.append(round(pf_t.max_drawdown()*100,3))
+    difference_in_maxdrawdown.append(round(min_maxdrawdown_values[i]-realized_maxdrawdown[i],3))
+
+# Lists for showing the hypothetical highest possible results with the strategy
+average_return_values_h, max_return_values_h, max_return_params_h = [],[],[]
+average_sharpe_values_h, max_sharpe_values_h, max_sharpe_params_h = [],[],[]
+average_maxdrawdown_values_h, min_maxdrawdown_values_h, min_maxdrawdown_params_h = [],[],[]
+
+# Finds the optimized parameters and results for the out-of-sample windows for comparison purposes
+for i in range(num_windows):
+    res_h = ind.run(
+        out_price[i], 
+        rsi_period = np.arange(5,7,step=1,dtype=int),
+        ma_period = np.arange(80,100,step=10,dtype=int),
+        entry = np.arange(28,32,step=2,dtype=float),
+        exit = np.arange(68,72,step=2,dtype=float),
+        param_product = True
+    )
+
+    entries_h = res_h.value == 1.0
+    exits_h = res_h.value == -1.0
+    
+    pf_h = vbt.Portfolio.from_signals(
+        out_price[i], 
+        entries_h, 
+        exits_h, 
+        **pf_kwargs
+    )
+
+    average_return_values_h.append(round(pf_h.total_return().mean()*100,3))
+    max_return_values_h.append(round(pf_h.total_return().max()*100,3))
+    max_return_params_h.append(pf_h.total_return().idxmax())
+
+    average_sharpe_values_h.append(round(pf_h.sharpe_ratio().mean(),3))
+    max_sharpe_values_h.append(round(pf_h.sharpe_ratio().max(),3))
+    max_sharpe_params_h.append(pf_h.sharpe_ratio().idxmax())
+
+    average_maxdrawdown_values_h.append(round(pf_h.max_drawdown().mean()*100,3))
+    min_maxdrawdown_values_h.append(round(pf_h.max_drawdown().min()*100,3))
+    min_maxdrawdown_params_h.append(pf_h.max_drawdown().idxmin())
+
+
+    # param_volume= pf_h.total_return().vbt.volume(
+    #     x_level = '_entry',
+    #     y_level = '_exit',
+    #     z_level = '_ma_period',
+    #     slider_level = '_rsi_period',
+    #     trace_kwargs=dict(colorbar=dict(title="Total return", tickformat='%'))
+    # )
+    # param_volume.show()
 
 # Results achieved from using parameter optimization.
-print(f'Average Return =  {round(mean(realized_returns),4)}%')
-print(f'Annualized Return =  {round(sum(realized_returns)*(261/(num_days/num_windows)),4)}%')
-print(f'Return by window (%):  {realized_returns}')
-print(f'Missed profit:  {missed_returns}')
-# print(f'MAD = {round(mean_ad,3)}')
+print(f'average return =  {round(mean(realized_returns),3)}%')
+print(f'annualized return =  {round(sum(realized_returns)*(261/(num_days/num_windows)),3)}%')
+print(f'average difference in return = {round(mean(difference_in_returns),3)}%')
 
-average_return_values = pd.DataFrame(average_return_values,columns=["In-sample Average Return (%)"])
-max_return_values = pd.DataFrame(max_return_values,columns=["In-sample Maximized Return (%)"])
-max_return_params = pd.DataFrame(max_return_params,columns=["Parameter 1","Parameter 2","Parameter 3","Parameter 4"])
+print(f'average Sharpe ratio =  {round(mean(realized_sharpe),3)}')
+print(f'average difference in Sharpe ratio = {round(mean(difference_in_sharpe),3)}')
 
-max_sharpe_params = pd.DataFrame(max_sharpe_params, columns=["Parameter 1","Parameter 2","Parameter 3","Parameter 4"])
+print(f'average max drawdown =  {round(mean(realized_maxdrawdown),3)}%')
+print(f'average difference in max drawdown = {round(mean(difference_in_maxdrawdown),3)}%')
 
-min_maxdrawdown_params = pd.DataFrame(min_maxdrawdown_params, columns=["Parameter 1","Parameter 2","Parameter 3","Parameter 4"])
 
-n = np.arange(1,num_windows+1)
-n = pd.DataFrame(n,columns=["Window"])
+# Converts the stats lists into dataframes for combining into tables
 
-metrics_df = pd.concat([n, average_return_values, max_return_values, max_return_params], axis=1)
+one_param_columns = ["Parameter 1"]
+two_param_columns = ["Parameter 1","Parameter 2"]
+three_param_columns = ["Parameter 1","Parameter 2","Parameter 3"]
+four_param_columns = ["Parameter 1","Parameter 2","Parameter 3","Parameter 4"]
+five_param_columns = ["Parameter 1","Parameter 2","Parameter 3","Parameter 4","Parameter 5"]
+
+column_list = four_param_columns
+
+
+realized_returns = pd.DataFrame(realized_returns, columns=["Realized Returns (%)"])
+difference_in_returns = pd.DataFrame(difference_in_returns, columns=["Difference from In-Sample (%)"])
+
+average_return_values = pd.DataFrame(average_return_values, columns=["In-Sample Average Return (%)"])
+max_return_values = pd.DataFrame(max_return_values, columns=["In-sample Maximized Return (%)"])
+max_return_params = pd.DataFrame(max_return_params, columns=column_list)
+
+average_return_values_h = pd.DataFrame(average_return_values_h, columns=["Out-of-Sample Average Return (%)"])
+max_return_values_h = pd.DataFrame(max_return_values_h, columns=["Out-of-Sample Maximized Return (%)"])
+max_return_params_h = pd.DataFrame(max_return_params_h, columns=column_list)
+
+
+realized_sharpe = pd.DataFrame(realized_sharpe, columns=["Realized Sharpe Ratio"])
+difference_in_sharpe = pd.DataFrame(difference_in_sharpe, columns=["Difference from In-Sample"])
+
+average_sharpe_values = pd.DataFrame(average_sharpe_values, columns=["In-Sample Average Sharpe Ratio"])
+max_sharpe_values = pd.DataFrame(max_sharpe_values, columns=["In-sample Maximized Sharpe Ratio"])
+max_sharpe_params = pd.DataFrame(max_sharpe_params, columns=column_list)
+
+average_sharpe_values_h = pd.DataFrame(average_sharpe_values_h, columns=["Out-of-Sample Average Sharpe Ratio"])
+max_sharpe_values_h = pd.DataFrame(max_sharpe_values_h, columns=["Out-of-Sample Maximized Sharpe Ratio"])
+max_sharpe_params_h = pd.DataFrame(max_sharpe_params_h, columns=column_list)
+
+
+realized_maxdrawdown = pd.DataFrame(realized_maxdrawdown, columns=["Realized Max Drawdown (%)"])
+difference_in_maxdrawdown = pd.DataFrame(difference_in_maxdrawdown, columns=["Difference from In-Sample (%)"])
+
+average_maxdrawdown_values = pd.DataFrame(average_maxdrawdown_values, columns=["In-Sample Average Max Drawdown (%)"])
+min_maxdrawdown_values = pd.DataFrame(min_maxdrawdown_values, columns=["In-sample Minimized Max Drawdown (%)"])
+min_maxdrawdown_params = pd.DataFrame(min_maxdrawdown_params, columns=column_list)
+
+average_maxdrawdown_values_h = pd.DataFrame(average_maxdrawdown_values_h, columns=["Out-of-Sample Average Max Drawdown (%)"])
+min_maxdrawdown_values_h = pd.DataFrame(min_maxdrawdown_values_h, columns=["Out-of-sample Minimized Max Drawdown (%)"])
+min_maxdrawdown_params_h = pd.DataFrame(min_maxdrawdown_params_h, columns=column_list)
+
+# Combines dataframes for the chosen metric into a new dataframe to be displayed in a dash table
+n = np.arange(1, num_windows+1)
+n = pd.DataFrame(n, columns=["Window"])
+
+insample_df = pd.concat([n, realized_returns, difference_in_returns, average_return_values, max_return_values, max_return_params], axis=1)
+
+outsample_df = pd.concat([n, average_return_values_h, max_return_values_h, max_return_params_h], axis=1)
--- src/components/tabs.py
+++ src/components/tabs.py
@@ -1,5 +1,5 @@
 from dash import html, dcc, dash_table
-from .. backtesting.simulation import metrics_df
+from .. backtesting.simulation import insample_df, outsample_df
 
 tabs = html.Div(
     [
@@ -35,11 +35,9 @@ parameters_tabs = html.Div(
     className="dbc"
 )
 
-#window_table = dash_table.DataTable(metrics_df.to_dict('records'))
-
-window_table = dash_table.DataTable(
-    data=metrics_df.to_dict('records'),
-    columns=[{'name': str(i), 'id': str(i)} for i in metrics_df.columns],
+insample_table = dash_table.DataTable(
+    data=insample_df.to_dict('records'),
+    columns=[{'name': str(i), 'id': str(i)} for i in insample_df.columns],
     style_as_list_view=True,
     style_header={
         'backgroundColor': 'rgb(30, 30, 30)',
